468f4f22c0e810dff11b51c8aa0f3c50c0ee78b6
Angella-Goot <angella.gutierrez01@gmail.com>
2026-07-20 22:32:55 -0500

    DOCS: Search bar inputs connected between pages, and fully dynamic to api :)

--- search.js
+++ search.js
@@ -1,26 +1,69 @@
 // GLOBAL VARIABLES
-const params = new URLSearchParams(window.location.search);
-const search = params.get("search");
 
 const searchInput = document.querySelector(".search__input")
 const searchBtn = document.querySelector(".search__btn")
 
-// API ARRAY FETCH
+const params = new URLSearchParams(window.location.search)
+const search = params.get("search")
+
+const resultListElm = document.querySelector(".result")
+
+let movies = []
+
+// API ARRAY FETCH - DOWNLOAD THE DATA
+
 async function fetchMovies() {
     const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1114bf7d&s=zombie")
     const movieData = await response.json()
-    const movies = movieData.Search
+    movies = movieData.Search
 
-    console.log(movies)
+    if (search) {
+        searchInput.value = search
+        filterMovies(search)
+    }
+    else {displayMovies(movies)}
+
+    console.log(movies) 
 }
 
 fetchMovies()
 
-console.log(search)
+// DISPLAY ARRAY
 
-// SEARCH BARS
+function displayMovies(movieArray) {
+    resultListElm.innerHTML = movieArray
+    .map(movie => renderMovie(movie))
+    .join('')
+}
+
+// RENDER MOVIE - TURN EACH MOVIE OBJECT INTO HTML
+
+function renderMovie(movie) {
+    return `<div class="result">
+                <div class="result__container">
+                    <img src="${movie.Poster}" alt="Poster" class="poster__img">
+                    <p class="movie__title">${movie.Title}</p>
+                    <p class="movie__date">${movie.Year}</p>
+                </div>
+            </div>`
+}
+
+// SEARCH BAR
+
 searchBtn.addEventListener("click", () => {
     const search = searchInput.value
 
-    window.location.href = `search.html?search=${search}`
+    filterMovies(search)
+    console.log(search)
 })
+
+function filterMovies(searchTerm) {
+    const filteredMovies = movies.filter((movie) => {
+        return (movie.Title.toLowerCase().includes(searchTerm.toLowerCase())) || (movie.Year.includes(searchTerm))
+    })
+
+    displayMovies(filteredMovies)
+}
+
+
+
